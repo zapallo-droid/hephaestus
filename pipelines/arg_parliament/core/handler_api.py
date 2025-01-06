48d45dffffdcdc631c49bc18040b263565e32dfc
Jesus Alcala <134946259+zapallo-droid@users.noreply.github.com>
2025-01-05 20:44:39 -0500

    TaskType Updated
    TaskType changed from UUID to Text as data type to allow more intuitive codes.

--- pipelines/arg_parliament/core/handler_api.py
+++ pipelines/arg_parliament/core/handler_api.py
@@ -7,6 +7,7 @@ from typing import Any, Optional
 from core.utils.handler_bucket import BucketHandler
 from core.model.jobs import Task
 
+
 logging.basicConfig(level=logging.INFO)
 
 class APIClientCall:
@@ -131,7 +132,7 @@ class APIExtractor(Task):
         self.task_exception = None
 
         super().__init__(job_id=self.job_id, name=self.name, pipeline_code=self.pipeline_code,
-                         source_code=self.source_code, location=self.location, task_type_code='7ae81b6c-9a4f-4582-be4d-1f14cc14b269')
+                         source_code=self.source_code, location=self.location, task_type_code='E')
 
     def run(self, job):
         logging.info(f'Working on: {self.data_source}')
