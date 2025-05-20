a9a65c57fed22e6e9b1dae6fe01b4dd78172ed5a
Jesus Alcala <134946259+zapallo-droid@users.noreply.github.com>
2025-05-19 23:18:54 -0400

    Setup Errors Fixed
    Setup Errors Fixed

--- setup.py
+++ setup.py
@@ -3,8 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name="hephaestus_pframe",
     version="0.1.0",
-    packages=find_packages(where="hephaestus_pframe"),
-    package_dir={"": "hephaestus_pframe"},
+    packages=find_packages(),
     install_requires=[
         "numpy",
         "psutil",
